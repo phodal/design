793fb72ca64445a2cfdb73fdf59b88ffc9574bfc
Phodal Huang <h@phodal.com>
2019-11-24 20:35:19 +0800

    feat: add basic flows

--- adapter/visual/sketch/index.js
+++ adapter/visual/sketch/index.js
@@ -6,12 +6,19 @@ function buildSketch(designInfo) {
     const page = new Page({
         name: 'HomePage'
     });
+    var index = 1;
     for (const key in designInfo.components) {
         const artboard = new Artboard({
-            name: key
+            name: key,
+            frame: {
+                width: '320px',
+                height: '480px',
+                x: 360 * index + 'px'
+            },
         });
 
         page.addArtboard(artboard);
+        index++;
     }
     newSketch.addPage(page);
     // const page = new Page({});
